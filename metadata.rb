91b67ccb46d39931f1a152e97da3bd621567cd2b
Brian Andrus <oddomatik@users.noreply.github.com>
2020-01-03 13:50:13 -0800

    increment version
    revert mistaken apache version increment too.

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures the Ceph distributed filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.35'
+version '0.9.37'
 
-depends	'apache2', '>= 1.1.13'
+depends	'apache2', '>= 1.1.12'
 depends 'apt'
 depends 'yum', '>= 3.0'
 depends 'yum-epel'
